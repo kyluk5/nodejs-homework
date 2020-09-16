1db79e893d21f50d1d9749dbdebba3a598efc9c4
Andrii Kulyk <kyluk5@gmail.com>
2020-09-16 11:47:45 +0300

    done

--- api/server.js
+++ api/server.js
@@ -35,10 +35,15 @@ module.exports = class CRUDServer {
   }
 
   async initDataBase() {
-    await mongoose.connect(process.env.MONGO_DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    try {
+      await mongoose.connect(process.env.MONGO_DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
   }
 
   initErrorHandling() {
